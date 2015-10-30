b42484de4b89a99b8dca3766e2c081b9759a251a
[Rick Zucker] <[rnz@uw.edu]>
2015-10-30 12:12:10 -0700

    Playing around with reading PDF files.

--- madlib.py
+++ madlib.py
@@ -1,51 +1,48 @@
-"""Practicing some basic web scraping
+"""Playing around with Python text modules to do MadLibs
 
-I am using the O'Reilly book, "Web Scraping with Python" by Ryan Mitchell.
-It's a very readable book for a technical book.
+This will read a webpage, preferably a newspaper article.
+It will produce a version with random words removed, that
+can be filled out for fun. It will also produce a version
+with words replaced with the same part of speech: adverb,
+adjective, verb, or noun.
 
-This will just be basic demonstrations. No deep scraping of a website(s)
-as I don't want to get in trouble.
+default URL, if none passed in is an article from the NY Times:
+http://www.nytimes.com/2015/10/27/upshot/trust-your-eyes-a-hot-streak-is-not-a-myth.html
 """
 
 from urllib.request import urlopen
 from urllib.error import HTTPError
-from bs4 import BeautifulSoup
+import PyPDF2
 import sys, logging, requests
+import os
 
 __author__ = 'rnzucker'
 
 logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
-def id_spoof():
-    """Simple browser agent spoofing
-    """
-    session = requests.Session()
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0",
-               "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
-    # Check to
-    url = "https://www.whatismybrowser.com/developers/what-http-headers-is-my-browser-sending"
-    req = session.get(url, headers=headers)
 
-    bsObj = BeautifulSoup(req.text, "html.parser")
-    print(bsObj.find("table",{"class":"table-striped"}).get_text)
+def text_list(filename):
+    """Takes a PDF filename and returns the text in the file
+
+    """
+    input_file = open(filename, "rb")
+    print("Opened", filename)
+    input = PyPDF2.PdfFileReader(input_file)
+    text = ""
+    i = 0
+    for page in input.pages:
+        text += page.extractText()
+        i += 1
+        if i > 10:
+            return text
+    return text
 
 def main():
-    # id_spoof()
-    # try:
-    #     html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-    # except HTTPError as e:
-    #     print(e)
-    #     return
-    # if html is None:
-    #     print("URL is not found")
-    #     return
-    r = requests.get("http://www.pythonscraping.com/pages/page1.html")
-    # bsObj = BeautifulSoup(html.read(), "html.parser")
-    print(r.text)
-    bsObj = BeautifulSoup(r.text, "html.parser")
-    # print(bsObj.h1)
-    print(bsObj.body)
+    input_file = "test.pdf"
+    print(text_list(input_file))
 
 
-
-main()
+# Check for interactive session
+if __name__ == '__main__':
+    # execute main program
+    main()
